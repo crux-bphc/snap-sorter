4e6f969ea72aa7e2332a59d7f275f1dc024adcce
majimearun <arunachala0619@gmail.com>
2023-05-27 21:33:55 +0530

    feat: merge both get user functions (by user id and bits id)

--- server/main.py
+++ server/main.py
@@ -40,18 +40,18 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 # Get a user by id
 
 
-@app.get("/users/{user_id}")
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
-# Get a user by BITS ID
-@app.get("/users/bits_id/{bits_id}")
-def read_user_by_bits_id(bits_id: str, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_bits_id(db, bits_id=bits_id)
+@app.get("/users/")
+def read_user(
+    user_id: Annotated[int | None, Query()] = None,
+    bits_id: Annotated[str | None, Query()] = None,
+    using_bits_id: bool = False,
+    db: Session = Depends(get_db),
+):
+    db_user = None
+    if using_bits_id:
+        db_user = crud.get_user_by_bits_id(db, bits_id=bits_id)
+    else:
+        db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
