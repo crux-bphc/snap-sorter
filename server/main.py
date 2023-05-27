8e2d5e41049c334725ea4ee6fec455a058423564
majimearun <arunachala0619@gmail.com>
2023-05-27 21:29:40 +0530

    refactor: comment out get user by email (as not required)

--- server/main.py
+++ server/main.py
@@ -58,12 +58,12 @@ def read_user_by_bits_id(bits_id: str, db: Session = Depends(get_db)):
 
 
 # Get a user by email
-@app.get("/users/email/{email}")
-def read_user_by_email(email: str, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=email)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+# @app.get("/users/email/{email}")
+# def read_user_by_email(email: str, db: Session = Depends(get_db)):
+#     db_user = crud.get_user_by_email(db, email=email)
+#     if db_user is None:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     return db_user
 
 
 # Image endpoints
