cf4785898f13ffb365d6db06e863ef2d3702e44e
majimearun <arunachala0619@gmail.com>
2023-05-27 15:30:28 +0530

    feat: complete getting user by their id (pk)

--- server/main.py
+++ server/main.py
@@ -34,3 +34,35 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     if db_user:
         raise HTTPException(status_code=400, detail="User already registered")
     return crud.create_user(db=db, user=user)
+
+
+# Get a user by id
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+# # Get a user by BITS ID
+# def read_user_by_bits_id(bits_id: str, db: Session = Depends(get_db)):
+#     db_user = crud.get_user_by_bits_id(db, bits_id=bits_id)
+#     if db_user is None:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     return db_user
+
+# # Get a user by email
+# def read_user_by_email(email: str, db: Session = Depends(get_db)):
+#     db_user = crud.get_user_by_email(db, email=email)
+#     if db_user is None:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     return db_user
+
+# # Get all users
+# @app.get("/users/", response_model=list[schemas.User])
+# def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     users = crud.get_users(db, skip=skip, limit=limit)
+#     return users
