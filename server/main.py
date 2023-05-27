8d51532c877b7b44241bf85e6382e624a4eca007
majimearun <arunachala0619@gmail.com>
2023-05-27 15:34:47 +0530

    feat: complete create image

--- server/main.py
+++ server/main.py
@@ -65,8 +65,14 @@ def read_user_by_email(email: str, db: Session = Depends(get_db)):
     return db_user
 
 
-# # Get all users
-# @app.get("/users/", response_model=list[schemas.User])
-# def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     users = crud.get_users(db, skip=skip, limit=limit)
-#     return users
+# Image endpoints
+
+# Create an image
+
+
+@app.post("/images/", response_model=schemas.Image)
+def create_image(image: schemas.ImageCreate, db: Session = Depends(get_db)):
+    db_image = crud.get_image_by_filepath(db, filepath=image.filepath)
+    if db_image:
+        raise HTTPException(status_code=400, detail="Image already registered")
+    return crud.create_image(db=db, image=image)
