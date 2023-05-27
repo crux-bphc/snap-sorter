d71f6ec270ee3638c59c7e50daf13025b30fde7a
majimearun <arunachala0619@gmail.com>
2023-05-27 15:06:36 +0530

    feat: wrote a couple basic endpoints to test out stuff

--- server/main.py
+++ server/main.py
@@ -18,18 +18,9 @@ def get_db():
         db.close()
 
 
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
-
-
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
+@app.get("/")
+def root():
+    return {"Snap Sorter": "In Development"}
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
@@ -40,14 +31,52 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
+@app.get("/users/bits_id/{bits_id}", response_model=schemas.User)
+def read_user_by_bits_id(bits_id: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_bits_id(db, bits_id=bits_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
 
-@app.get("/items/", response_model=list[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
+@app.get("/users/email/{email}", response_model=schemas.User)
+def read_user_by_email(email: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=email)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_bits_id(db, bits_id=user.bits_id)
+    if db_user:
+        raise HTTPException(status_code=400, detail="User already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.post("/images/{image_id}/tag/{user_id}", response_model=schemas.Image)
+def tag_image(image_id: int, user_id: int, db: Session = Depends(get_db)):
+    db_image = crud.get_image(db, image_id=image_id)
+    if db_image is None:
+        raise HTTPException(status_code=404, detail="Image not found")
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return crud.tag_image(db=db, image_id=image_id, user_id=user_id)
+
+
+@app.get("/images/{image_id}", response_model=schemas.Image)
+def read_image(image_id: int, db: Session = Depends(get_db)):
+    db_image = crud.get_image(db, image_id=image_id)
+    if db_image is None:
+        raise HTTPException(status_code=404, detail="Image not found")
+    return db_image
+
+
+@app.get("/images/{user_id}/tagged", response_model=list[schemas.Image])
+def read_tagged_images(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return crud.get_tagged_images(db=db, user_id=user_id)
