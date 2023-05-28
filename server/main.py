6724ae8d9c7ed8741c7f133af2a64e199bd30104
majimearun <arunachala0619@gmail.com>
2023-05-28 13:17:45 +0530

    feat: add annotations and some basic validation

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 from typing import Annotated
-from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi import Body, Depends, FastAPI, HTTPException, Path, Query
 from sqlalchemy.orm import Session
+import re
 
 import crud, models, schemas
 from database import SessionLocal, engine
@@ -21,7 +22,7 @@ def get_db():
 
 @app.get("/")
 def root():
-    return {"Snap Sorter": "In Development"}
+    return {"Message": "Please go to /docs, to test the API"}
 
 
 # User endpoints
@@ -29,26 +30,59 @@ def root():
 # Create a user
 
 
-@app.post("/users/")
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+@app.post("/users/", response_model=schemas.User)
+def create_user(
+    db: Annotated[Session, Depends(get_db)],
+    user: Annotated[
+        schemas.UserCreate,
+        Body(
+            ...,
+            title="User definition",
+            description="Object that gets converted to a user",
+        ),
+    ],
+):
     db_user = crud.get_user_by_bits_id(db, bits_id=user.bits_id)
     if db_user:
         raise HTTPException(status_code=400, detail="User already registered")
     return crud.create_user(db=db, user=user)
 
 
-# Get a user by id
+# Get a user by id (or) bits_id
+
+# example bits_id: 2021A7PS0001H
 
 
 @app.get("/users/")
 def read_user(
-    user_id: Annotated[int | None, Query()] = None,
-    bits_id: Annotated[str | None, Query()] = None,
-    using_bits_id: bool = False,
-    db: Session = Depends(get_db),
+    db: Annotated[Session, Depends(get_db)],
+    user_id: Annotated[
+        int | None,
+        Query(alias="user-id", title="User ID", description="User ID"),
+    ] = None,
+    bits_id: Annotated[
+        str | None,
+        Query(
+            alias="bits-id",
+            title="BITS ID",
+            description="Your BITS ID number",
+        ),
+    ] = None,
+    using_bits_id: Annotated[
+        bool,
+        Query(
+            title="Using BITS ID",
+            description="Boolean to indicate whether to use BITS ID or User ID",
+        ),
+    ] = False,
 ):
     db_user = None
     if using_bits_id:
+        check = re.match("^\d{4}[A-Z][1-7][P|T]S\d{4}H$", bits_id) or re.match(
+            "^\d{4}[A-B][1-7][A-B][1-7]\d{4}H$", bits_id
+        )
+        if not check:
+            raise HTTPException(status_code=400, detail="Invalid BITS ID")
         db_user = crud.get_user_by_bits_id(db, bits_id=bits_id)
     else:
         db_user = crud.get_user(db, user_id=user_id)
@@ -57,22 +91,23 @@ def read_user(
     return db_user
 
 
-# Get a user by email
-# @app.get("/users/email/{email}")
-# def read_user_by_email(email: str, db: Session = Depends(get_db)):
-#     db_user = crud.get_user_by_email(db, email=email)
-#     if db_user is None:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     return db_user
-
-
 # Image endpoints
 
 # Create an image
 
 
 @app.post("/images/")
-def create_image(image: schemas.ImageCreate, db: Session = Depends(get_db)):
+def create_image(
+    db: Annotated[Session, Depends(get_db)],
+    image: Annotated[
+        schemas.ImageCreate,
+        Body(
+            ...,
+            title="Image definition",
+            description="Object that gets converted to an image",
+        ),
+    ],
+):
     db_image = crud.get_image_by_filepath(db, filepath=image.filepath)
     if db_image:
         raise HTTPException(status_code=400, detail="Image already registered")
@@ -83,7 +118,18 @@ def create_image(image: schemas.ImageCreate, db: Session = Depends(get_db)):
 
 
 @app.get("/images/{image_id}")
-def read_image(image_id: int, db: Session = Depends(get_db)):
+def read_image(
+    db: Annotated[Session, Depends(get_db)],
+    image_id: Annotated[
+        int,
+        Path(
+            ...,
+            gt=0,
+            title="Image ID",
+            description="Image ID based on which the image is fetched",
+        ),
+    ],
+):
     db_image = crud.get_image(db, image_id=image_id)
     if db_image is None:
         raise HTTPException(status_code=404, detail="Image not found")
@@ -93,11 +139,24 @@ def read_image(image_id: int, db: Session = Depends(get_db)):
 # Tag an image
 
 
-@app.post("/images/tag")
+@app.post("/images/tag/")
 def tag_image(
-    image_id: Annotated[int, Query(...)],
-    user_id: Annotated[int, Query(...)],
-    db: Session = Depends(get_db),
+    db: Annotated[Session, Depends(get_db)],
+    image_id: Annotated[
+        int,
+        Query(
+            ..., gt=0, title="Image ID", description="Image's ID which is to be tagged"
+        ),
+    ],
+    user_id: Annotated[
+        int,
+        Query(
+            ...,
+            gt=0,
+            title="User ID",
+            description="User's ID who is tagged in the image",
+        ),
+    ],
 ):
     db_image = crud.get_image(db, image_id=image_id)
     if db_image is None:
@@ -112,7 +171,18 @@ def tag_image(
 
 
 @app.get("/images/user/{user_id}")
-def read_images_by_user(user_id: int, db: Session = Depends(get_db)):
+def read_images_by_user(
+    db: Annotated[Session, Depends(get_db)],
+    user_id: Annotated[
+        int,
+        Path(
+            ...,
+            gt=0,
+            title="User ID",
+            description="User's ID whose images are to be fetched",
+        ),
+    ],
+):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
