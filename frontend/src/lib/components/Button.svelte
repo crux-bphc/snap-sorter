c74c0058050f0f34f1793265444ab0b1d2b04a40
pnicto <dharanikanth789@gmail.com>
2023-06-03 17:48:40 +0530

    feat: Finish button with variants

---
+++ frontend/src/lib/components/Button.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import clsx from 'clsx';
+
+	const colorVariants = {
+		primary: 'bg-primary-800 hover:bg-primary-700 text-primary-200',
+		yellow: 'bg-yellow-800 hover:bg-yellow-700 text-yellow-200',
+		orange: 'bg-yellow-800 hover:bg-yellow-700 text-orange-200',
+		purple: 'bg-purple-800 hover:bg-purple-700 text-purple-200',
+		error: 'bg-error-800 hover:bg-error-700 text-error-200',
+		success: 'bg-forestGreen-800 hover:bg-forestGreen-700 text-forestGreen-200'
+	};
+
+	export let type: 'button' | 'submit' | 'reset' = 'button';
+	export let padding: string = 'px-4 py-2';
+	export let margin: string = 'mx-1 my-1';
+	export let color: keyof typeof colorVariants = 'primary';
+
+	// Additional classes if needed.
+	// Need to be careful when using as the outcome may not be what one wants because of the way precendence in tailwind works
+	export let classes: string = '';
+</script>
+
+<button
+	{type}
+	on:click
+	class={clsx(colorVariants[color], padding, margin, 'rounded font-bold capitalize', classes)}
+	><slot /></button
+>
