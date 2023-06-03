62e5556e859ce5306cc1d9beca2ef6480601de0f
pnicto <dharanikanth789@gmail.com>
2023-06-03 21:20:06 +0530

    style: Format with prettier

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1 class="p-4 pt-2 text-3xl font-bold text-blue-500 underline">Welcome to SvelteKit</h1>
+<h1 class="text-blue-500 p-4 pt-2 text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
