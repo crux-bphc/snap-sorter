77c7696d21642d263ffbe1ef0829467857574306
pnicto <dharanikanth789@gmail.com>
2023-06-03 20:53:50 +0530

    feat: Finish footer

---
+++ frontend/src/lib/components/Footer.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import facebookIcon from '@iconify/icons-mdi/facebook';
+	import googleGmail from '@iconify/icons-mdi/gmail';
+	import githubIcon from '@iconify/icons-mdi/github';
+</script>
+
+<footer
+	class="container mx-auto flex items-center justify-between bg-primary-200 px-5 py-3 shadow-lg"
+>
+	<p class="text-5xl font-bold text-primary-600">cruX</p>
+	<ul class="grid list-none grid-cols-1 items-center font-semibold text-primary-600 sm:flex">
+		<li class="mx-2 hover:text-primary-900">
+			<a href="https://www.github.com/crux-bphc" rel="external" target="_blank">
+				<Icon icon={githubIcon} width="35" />
+			</a>
+		</li>
+		<li class="mx-2 hover:text-primary-900">
+			<a href="mailto:crux@hyderabad.bits-pilani.ac.in" rel="external" target="_blank">
+				<Icon icon={googleGmail} width="35" />
+			</a>
+		</li>
+		<li class="mx-2 hover:text-primary-900">
+			<a href="https://www.facebook.com/cruxbphc/" rel="external" target="_blank">
+				<Icon icon={facebookIcon} width="35" />
+			</a>
+		</li>
+	</ul>
+</footer>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+	import Footer from '$lib/components/Footer.svelte';
 	import '../app.css';
 </script>
 
 <slot />
+
+<!-- TODO: Position footer in the right place -->
+<Footer />
