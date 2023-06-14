f8a0e2644e980b4e88620015aa90d12f32c63cb6
Dharanikanth Reddy <f20210264@hyderabad.bits-pilani.ac.in>
2023-06-14 18:11:20 +0530

    feat: Add navbar to the profile

---
+++ frontend/src/lib/components/Navbar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+	import Button from './Button.svelte';
+	let mounted = false;
+	onMount(() => (mounted = true));
+</script>
+
+{#if mounted}
+	<nav class="flex justify-end" transition:fade>
+		<!-- TODO: Add a toggle for color scheme -->
+		<!-- TODO: search route page-->
+		<Button type="button" color="purple">
+			<a href="/search"> image search </a>
+		</Button>
+		<form use:enhance method="post">
+			<Button type="submit" color="error">Logout</Button>
+		</form>
+	</nav>
+{/if}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
+	import Navbar from '$lib/components/Navbar.svelte';
 	import type { PageData } from './$types';
-	import Button from '$lib/components/Button.svelte';
 	export let data: PageData;
 </script>
 
+<Navbar />
+
+<!-- TODO: Get name and profile from server.ts => providerUser to db -->
 <h1>Profile</h1>
 <h2>User id: {data.user.userId}</h2>
 <h2>Email: {data.user.email}</h2>
-
-<form use:enhance method="post">
-	<Button type="submit" color="primary">Logout</Button>
-</form>
