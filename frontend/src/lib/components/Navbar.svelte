d78610c8ff7ee815183bd6c52965923c521dc2cc
Dharanikanth Reddy <f20210264@hyderabad.bits-pilani.ac.in>
2023-06-17 18:30:37 +0530

    refactor: Add the remaining routes

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -11,6 +11,13 @@
 	<nav class="flex flex-wrap justify-center md:justify-end" transition:fade>
 		<!-- TODO: Add a toggle for color scheme -->
 		<!-- TODO: search route page-->
+		<!-- TODO: make it dynamic depending on login state and page-->
+		<Button type="button" color="yellow">
+			<a href="/profile">profile</a>
+		</Button>
+		<Button type="button" color="success">
+			<a href="/announcements">announcements</a>
+		</Button>
 		<Button type="button" color="purple">
 			<a href="/search"> image search </a>
 		</Button>
