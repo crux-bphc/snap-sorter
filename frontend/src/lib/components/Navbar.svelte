12f5861d158d7d241ac64b67dbd9e9fe786d12ce
Dharanikanth Reddy <f20210264@hyderabad.bits-pilani.ac.in>
2023-06-17 18:12:04 +0530

    fix: position navbar on center on smaller screens

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -8,7 +8,7 @@
 </script>
 
 {#if mounted}
-	<nav class="flex justify-end" transition:fade>
+	<nav class="flex justify-center md:justify-end" transition:fade>
 		<!-- TODO: Add a toggle for color scheme -->
 		<!-- TODO: search route page-->
 		<Button type="button" color="purple">
