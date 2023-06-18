667253847f11760db885760d4099b5f3a75d78e5
Dharanikanth Reddy <f20210264@hyderabad.bits-pilani.ac.in>
2023-06-18 21:08:21 +0530

    feat: make navbar logout button dynamic

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -3,6 +3,8 @@
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import Button from './Button.svelte';
+	import { page } from '$app/stores';
+
 	let mounted = false;
 	onMount(() => (mounted = true));
 </script>
@@ -11,11 +13,13 @@
 	<nav class=" flex w-screen flex-wrap justify-center md:justify-end" transition:fade>
 		<!-- TODO: Add a toggle for color scheme -->
 		<!-- TODO: search route page-->
-		<Button type="button" color="purple">
-			<a href="/search"> image search </a>
-		</Button>
-		<form use:enhance method="post" action="/profile?/logout" class="flex">
-			<Button type="submit" color="error">Logout</Button>
-		</form>
+		{#if $page.data.user !== null}
+			<Button type="button" color="purple">
+				<a href="/search"> image search </a>
+			</Button>
+			<form use:enhance method="post" action="/profile?/logout" class="flex">
+				<Button type="submit" color="error">Logout</Button>
+			</form>
+		{/if}
 	</nav>
 {/if}
