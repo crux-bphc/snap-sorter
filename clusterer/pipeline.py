131ba181ad82edd5f8b75f7c663c9d7d07e35137
shailx383 <shailx383@gmail.com>
2023-06-18 16:13:03 +0530

    fix: changed variable names

--- clusterer/pipeline.py
+++ clusterer/pipeline.py
@@ -1,10 +1,10 @@
 from inference import EmbeddingPipeline
-import datapoint_clusterer as cl
+import datapoint_clusterer as clusterer
 import os
 import torch
 import pickle
 
-def cluster_from_file(src_path: str, dest_path: str, eps: float):
+def cluster_from_file(source_path: str, dest_path: str, epsilon: float):
     '''
     creates clusters from datapoints stored in src_path and writes them to dest_path
     
@@ -16,40 +16,43 @@ def cluster_from_file(src_path: str, dest_path: str, eps: float):
     Returns: 
         None
     '''
-    points = cl.read_datapoints(src_path)
-    clusters = cl.create_clusters(points, eps)
-    cl.write_clusters(clusters, dest_path)
+    points = clusterer.read_datapoints(source_path)
+    clusters = clusterer.create_clusters(points, epsilon)
+    clusterer.write_clusters(clusters, dest_path)
     return None
         
-def write_datapoints(src_path: str, dest_path: str):
+def write_datapoints(source_path: str, dest_path: str):
     '''
     reads images from file or folder, creates datapoints and writes them into .pkl file
     
     Args:
         src_path (str): path of file or folder containing images
         dest_path (str): path of .pkl file to write datapoints into
+    
+    Returns:
+        None
     '''
     points = []
-    ep = EmbeddingPipeline()
-    if os.path.isfile(src_path):
-        embs = ep(src_path)
-        if embs is not None:
-            for emb in embs:
-                emb = torch.from_numpy(emb)
-                pt = cl.Datapoint(emb, None, src_path)
-                points.append(pt)
+    pipeline = EmbeddingPipeline()
+    if os.path.isfile(source_path):
+        embeddings = pipeline(source_path)
+        if embeddings is not None:
+            for embedding in embeddings:
+                embedding = torch.from_numpy(embedding)
+                point = clusterer.Datapoint(embedding, None, source_path)
+                points.append(point)
     else:
-        for root, _, f_names in os.walk(src_path):
-            for f in f_names:
-                imgpath = os.path.join(root, f)
-                embs = ep(imgpath)
-                if embs is not None:
-                    for emb in embs:
-                        emb = torch.from_numpy(emb)
-                        pt = cl.Datapoint(emb, None, imgpath)
-                        points.append(pt)
+        for root, _, filenames in os.walk(source_path):
+            for filename in filenames:
+                imgpath = os.path.join(root, filename)
+                embeddings = pipeline(imgpath)
+                if embeddings is not None:
+                    for embedding in embeddings:
+                        embedding = torch.from_numpy(embedding)
+                        point = clusterer.Datapoint(embedding, None, imgpath)
+                        points.append(point)
     with open(dest_path, 'wb') as f:
         pickle.dump(points, f, protocol=pickle.HIGHEST_PROTOCOL)
     print("Written succesfully!")
     return None
-        
+
