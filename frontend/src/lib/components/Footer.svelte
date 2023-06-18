c30f172171d7b6d784c341b115f92c5dbf7b1e22
Dharanikanth Reddy <f20210264@hyderabad.bits-pilani.ac.in>
2023-06-18 21:07:55 +0530

    style: format with prettier

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -7,22 +7,22 @@
 </script>
 
 <footer
-	class="bg-primary-200 text-primary-700 flex w-screen items-center justify-between px-5 py-3 shadow-lg"
+	class="flex w-screen items-center justify-between bg-primary-200 px-5 py-3 text-primary-700 shadow-lg"
 	transition:fade
 >
 	<p class="text-5xl font-bold">cruX</p>
 	<ul class="grid list-none grid-cols-1 items-center font-semibold sm:flex">
-		<li class="hover:text-primary-900 mx-2">
+		<li class="mx-2 hover:text-primary-900">
 			<a href="https://www.github.com/crux-bphc" rel="external" target="_blank">
 				<Icon icon={githubIcon} width="35" />
 			</a>
 		</li>
-		<li class="hover:text-primary-900 mx-2">
+		<li class="mx-2 hover:text-primary-900">
 			<a href="mailto:crux@hyderabad.bits-pilani.ac.in" rel="external" target="_blank">
 				<Icon icon={googleGmail} width="35" />
 			</a>
 		</li>
-		<li class="hover:text-primary-900 mx-2">
+		<li class="mx-2 hover:text-primary-900">
 			<a href="https://www.facebook.com/cruxbphc/" rel="external" target="_blank">
 				<Icon icon={facebookIcon} width="35" />
 			</a>
--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -13,12 +13,12 @@
 
 {#if loaded}
 	<main class="m-auto flex flex-col items-center justify-center text-center" transition:fade>
-		<h1 class="text-primary-600 m-4 text-7xl font-bold">Snap Sorter</h1>
+		<h1 class="m-4 text-7xl font-bold text-primary-600">Snap Sorter</h1>
 		<Button type="submit" color="primary" size="large"
 			><a href="/api/oauth" class="flex items-center gap-3"
 				>Login with Google <Icon icon={googleIcon} /></a
 			></Button
 		>
-		<p class="text-primary-500 text-[0.8rem] font-bold">*Use your BITS email to login</p>
+		<p class="text-[0.8rem] font-bold text-primary-500">*Use your BITS email to login</p>
 	</main>
 {/if}
