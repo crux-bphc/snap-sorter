25f68d4b330ba47e1e8669eee2941a9efee4443a
Dharanikanth Reddy <f20210264@hyderabad.bits-pilani.ac.in>
2023-06-24 09:13:56 +0530

    feat: add clear selection, some minor changes to position of button
    Also added a todo for delete button for each image on hover

--- frontend/src/routes/profile/+page.svelte
+++ frontend/src/routes/profile/+page.svelte
@@ -61,9 +61,14 @@
 <main class="align-between m-auto flex flex-col gap-5">
 	<div>
 		<h1 class="my-3 text-3xl font-bold">Profile:</h1>
-		<h2>User ID: {data.user.userId}</h2>
 		<h2>Email: {data.user.email}</h2>
 		<h2>Name: {data.user.name}</h2>
+	</div>
+	<div class="mx-auto">
+		<Button type="submit" on:click={uploadFiles} color="success" size="small">Upload</Button>
+		<Button type="submit" on:click={() => (images = [])} color="error" size="small"
+			>clear selection</Button
+		>
 	</div>
 	<div>
 		<h1 class="my-3 text-xl font-semibold">Image Upload:</h1>
@@ -75,17 +80,25 @@
 				</li>
 			{/each}
 		</ol>
-		<h2 class="mb-2 mt-4 text-lg font-semibold">Selected Images:</h2>
-		<div class="mb-4 grid grid-cols-3 place-items-stretch gap-4">
-			{#each images as { src, height, width, name, size }}
-				<div class="flex w-52 flex-col items-center justify-center gap-2 bg-purple-50 p-2">
-					<img alt="" {src} class="max-h-48" />
-					<p class="text-center text-sm font-semibold text-purple-500">
-						{name} ({(size / 1000000).toFixed(2)} MB)
-					</p>
-				</div>
-			{/each}
-		</div>
-		<Button type="submit" on:click={uploadFiles} color="success" size="small">Upload</Button>
+		{#if images.length > 0}
+			<h2 class="mb-2 mt-4 text-lg font-semibold">Selected Images:</h2>
+			<div
+				class="mb-4 grid grid-cols-2 place-items-stretch gap-4 bg-primary-200 sm:grid-cols-3 md:grid-cols-4"
+			>
+				{#each images as { src, height, width, name, size }}
+					<div
+						class="flex w-52 flex-col items-center justify-center gap-2 bg-purple-50 p-2"
+						on:mouseenter={() => {
+							// TODO: Add delete button/icon for each image
+						}}
+					>
+						<img alt={name} {src} class="max-h-48" />
+						<p class="text-center text-sm font-semibold text-purple-500">
+							{name} ({(size / 1000000).toFixed(2)} MB)
+						</p>
+					</div>
+				{/each}
+			</div>
+		{/if}
 	</div>
 </main>
