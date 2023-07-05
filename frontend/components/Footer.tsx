37d387be6e9994dca04228fba0c682d4329b86a0
Dharanikanth Reddy <f20210264@hyderabad.bits-pilani.ac.in>
2023-07-05 09:46:36 +0530

    refactor: to make the footer look less padded

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -3,9 +3,9 @@ import { Icon } from "@iconify/react";
 
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-between px-10 text-4xl">
-      <p className="text-3xl">cruX</p>
-      <ul className="list-none flex items-center pt-2">
+    <footer className="flex items-center justify-between px-10 text-4xl py-3">
+      <p className="text-3xl py-0 my-0">cruX</p>
+      <ul className="list-none flex items-center py-0 my-0 justify-evenly gap-x-2">
         <li>
           <Anchor target="_blank" href="">
             <Icon icon="mdi:github" />
