71c7012ffb6cf7624661f25c5b50bf22b75375f3
Dharanikanth Reddy <f20210264@hyderabad.bits-pilani.ac.in>
2023-07-05 23:49:22 +0530

    feat: image uploads from profile

---
+++ frontend/pages/app/profile.tsx
@@ -0,0 +1,89 @@
+// File which handles the user profile
+// Pitfalls:
+// <p> tags margin reset
+// Possibly remove <Group> and <Image> from mantine and use regular html tags
+// !There's no max file size limit or max number of files limit
+import BaseLayout from "@/components/layouts/BaseLayout";
+import { useState } from "react";
+import { Image, Text, Group, Button } from "@mantine/core";
+import { Dropzone, FileWithPath } from "@mantine/dropzone";
+
+// TODO: Replace with data from OAuth
+const userData = {
+  email: "email@email.com",
+  name: "Kovid Lakhera",
+};
+
+export default function Profile() {
+  const [images, setImages] = useState<FileWithPath[]>([]);
+
+  const handleImageUpload = async function () {
+    // TODO: Finish image upload request
+    console.log(images);
+  };
+
+  const previews = images.map((file, index) => {
+    const imageUrl = URL.createObjectURL(file);
+
+    return (
+      <Image
+        key={index}
+        alt={imageUrl}
+        src={imageUrl}
+        imageProps={{ onLoad: () => URL.revokeObjectURL(imageUrl) }}
+      />
+    );
+  });
+
+  return (
+    <BaseLayout>
+      <main className="py-7 px-10">
+        <div className="flex flex-col container mx-auto text-center">
+          <section>
+            <h1 className="text-3xl my-2">Profile</h1>
+            <p className="m-1">
+              <b>Name:</b> {userData.name}
+            </p>
+            <p className="m-1">
+              <b>Email:</b> {userData.email}
+            </p>
+          </section>
+
+          <section>
+            <Group position="center" className="py-5">
+              <Button
+                type="submit"
+                color="green"
+                onClick={handleImageUpload}
+                disabled={images.length === 0}
+              >
+                Upload
+              </Button>
+              <Button
+                type="button"
+                color="red"
+                onClick={() => setImages([])}
+                disabled={images.length === 0}
+              >
+                Reset
+              </Button>
+            </Group>
+            <Dropzone accept={["image/jpeg", "image/png"]} onDrop={setImages}>
+              <Text align="center">Drop images here</Text>
+            </Dropzone>
+          </section>
+
+          <section>
+            {previews.length > 0 && (
+              <div
+                className={`grid grid-cols-1 py-4 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-5 lg:grid-cols-4 xl:grid-cols-5`}
+              >
+                {previews}
+              </div>
+            )}
+          </section>
+        </div>
+      </main>
+    </BaseLayout>
+  );
+}
