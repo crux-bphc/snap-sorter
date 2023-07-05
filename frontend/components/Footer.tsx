1c49b64544e5c32106615540a466881b626ff726
Dharanikanth Reddy <f20210264@hyderabad.bits-pilani.ac.in>
2023-07-05 09:47:01 +0530

    docs: add todo,  comments on spacing

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,6 +1,8 @@
+// Here the padding y on footer is responsible for the spacing
 import { Anchor } from "@mantine/core";
 import { Icon } from "@iconify/react";
 
+// TODO: Add urls for socials
 export default function Footer() {
   return (
     <footer className="flex items-center justify-between px-10 text-4xl py-3">
