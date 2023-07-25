3f1caf7473c300cee9edc114e69f2bdd60020c45
skoriop <karthikp.ivy@gmail.com>
2023-07-25 16:37:40 +0000

    feat: add prisma and postgres

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,13 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import "dotenv/config";
 
+const prisma = new PrismaClient();
+
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID ?? "",
