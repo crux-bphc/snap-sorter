fddff226d41160bb290dc7c23c7824af1bce84ae
skoriop <karthikp.ivy@gmail.com>
2023-07-25 11:25:59 +0000

    feat: add auth

---
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import "dotenv/config";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
