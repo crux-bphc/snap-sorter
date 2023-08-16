eb3fcaf01ec20ce836b0c9d85cfc71daa7f7ec3c
skoriop <karthikp.ivy@gmail.com>
2023-08-16 11:26:19 +0000

    feat: hide current page in navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -4,9 +4,11 @@
 import { Anchor, Button } from "@mantine/core";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
 	const { data: session } = useSession();
+	const router = useRouter();
 
 	const protectedRoutes = [
 		{
@@ -33,16 +35,9 @@ export default function Navbar() {
 				Logo
 			</Anchor>
 			<ul className="flex list-none px-0">
-				{unprotectedRoutes.map((route) => (
-					<li className="mx-2">
-						<Button component={Link} href={route.path} className="capitalize">
-							{route.name}
-						</Button>
-					</li>
-				))}
-				{session?.user && (
-					<>
-						{protectedRoutes.map((route) => (
+				{unprotectedRoutes.map(
+					(route) =>
+						router.pathname !== route.path && (
 							<li className="mx-2">
 								<Button
 									component={Link}
@@ -51,7 +46,23 @@ export default function Navbar() {
 									{route.name}
 								</Button>
 							</li>
-						))}
+						)
+				)}
+				{session?.user && (
+					<>
+						{protectedRoutes.map(
+							(route) =>
+								router.pathname !== route.path && (
+									<li className="mx-2">
+										<Button
+											component={Link}
+											href={route.path}
+											className="capitalize">
+											{route.name}
+										</Button>
+									</li>
+								)
+						)}
 						<li className="mx-2">
 							<Button
 								onClick={() => signOut({ callbackUrl: "/login" })}
