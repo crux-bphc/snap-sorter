9cd67f667c433924e7b6fc4ae47a82098548d67a
Dharanikanth Reddy <f20210264@hyderabad.bits-pilani.ac.in>
2023-08-19 17:38:16 +0530

    refactor: replace logo with login button

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,7 +1,7 @@
 // Pitfalls:
 // Since tailwind preflight is disabled some of these styles overwrite the default styles or make use of the default styles
 // For example here 'px-0` is needed to remove the default unordered list padding
-import { Anchor, Button } from "@mantine/core";
+import { Button } from "@mantine/core";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -31,10 +31,12 @@ export default function Navbar() {
 	return (
 		<nav className="flex items-center justify-between px-10">
 			{/* TODO: Add crux logo here for home route */}
-			<Anchor component={Link} href={"/"}>
-				Logo
-			</Anchor>
 			<ul className="flex list-none px-0">
+				{router.pathname !== "/login" && session?.user === undefined && (
+					<Button component={Link} href={"/"} className="mx-2 capitalize">
+						login
+					</Button>
+				)}
 				{unprotectedRoutes.map(
 					(route, index) =>
 						router.pathname !== route.path && (
