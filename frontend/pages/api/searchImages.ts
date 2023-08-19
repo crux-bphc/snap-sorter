4f24ee396964c2366d173e293a828c8db2785b58
Dharanikanth Reddy <f20210264@hyderabad.bits-pilani.ac.in>
2023-08-19 15:21:05 +0530

    feat: finish search by event and uid

---
+++ frontend/pages/api/searchImages.ts
@@ -0,0 +1,53 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth/next";
+import { authOptions, prisma } from "./auth/[...nextauth]";
+import { DopyImage } from "@prisma/client";
+
+interface ExtendedNextApiRequest extends NextApiRequest {
+	body: {
+		uid: string;
+		events: string[];
+		eventYear: string;
+	};
+}
+
+export default async function handler(
+	req: ExtendedNextApiRequest,
+	res: NextApiResponse
+) {
+	const session = await getServerSession(req, res, authOptions);
+
+	if (req.method !== "POST") {
+		return res.status(405).send(`Method ${req.method} Not Allowed`);
+	}
+
+	if (!session) {
+		return res.status(401).send("Unauthorized");
+	}
+
+	// TODO: Handle req body here
+	const { events, eventYear, uid } = req.body;
+	console.log(req.body);
+
+	try {
+		const userImages: DopyImage[][] = [];
+		for (const event of events) {
+			const images = await prisma.dopyImage.findMany({
+				where: {
+					eventId: event,
+					taggedUsers: {
+						every: {
+							email: `${uid}@hyderabad.bits-pilani.ac.in`,
+						},
+					},
+				},
+			});
+			userImages.push(images);
+		}
+
+		res.status(200).json({ userImages });
+	} catch (e) {
+		console.error(e);
+		res.status(500).send("Internal Server Error");
+	}
+}
